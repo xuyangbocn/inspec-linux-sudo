94e1a02fdaa3b984c0ff348e3056e4837796d9b7
yangbo <xuyangbocn@gmail.com>
2020-09-23 16:48:36 +0800

    add more controls for sudos

--- controls/sudo-config.rb
+++ controls/sudo-config.rb
@@ -1,7 +1,5 @@
 # copyright: 2018, The Authors
 
-title "linux sudo related configurations"
-
 # # you add controls here
 # control "tmp-1.0" do                        # A unique ID for this control
 #   impact 0.7                                # The criticality, if this control fails.
@@ -12,11 +10,56 @@ title "linux sudo related configurations"
 #   end
 # end
 
-control "sudo-version" do
+control "CSG-SUDO-A1" do
     impact 0.7
-    title "check sudo version in rhel"
+    title "check sudo version in linux"
     describe package('sudo') do
       it { should be_installed }
-      its('version') { should eq '1.8.23-4.el7.x86_64' }
+      its('version') { should cmp >='1.8.23' }
     end
 end
+
+control "CSG-SUDO-B3" do
+    impact 0.7
+    title "not permit root login"
+    describe sshd_config do
+        its('PermitRootLogin') { should cmp 'no' }
+    end
+end
+
+control "CSG-SUDO-B7" do
+    impact 0.7
+    title "disable root access"
+    describe file('/etc/securetty') do
+      its('size') { should be 0 }
+    end
+end
+
+control "CSG-SUDO-C3" do
+    impact 0.7
+    title "only root account has UID 0"
+    describe passwd.uids(0) do
+      its('users') { should cmp 'root' }
+      its('count') { should eq 1 }
+    end
+end
+
+control "CSG-SUDO-C4" do
+    impact 0.7
+    title "only root account has GID 0"
+    describe passwd.gids(0) do
+      its('users') { should cmp 'root' }
+      its('count') { should eq 1 }
+    end
+end
+
+control "CSG-SUDO-E7" do
+    impact 0.7
+    title "sudoers file permission"
+    describe file('/etc/sudoers') do
+        its('mode') { should cmp '0440'}
+        its('owner') { should eq 'root' }
+    end
+end
+
+
